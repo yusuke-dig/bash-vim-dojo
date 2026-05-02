891e5f79a1d6306a17c9e9a9fbffe1cf5fca5220
YusukeYokota <yusuke_yokota_ab@mail.toyota.co.jp>
2026-05-02 11:26:51 +0900

    Day 11: exercise.sh 解答記入

--- lessons/day11/exercise.sh
+++ lessons/day11/exercise.sh
@@ -17,19 +17,24 @@ echo "cherry" >> data.txt
 # Task 1: data.txt の "apple" を "orange" に置換して result_task1.txt に保存する
 # ヒント: sed 's/apple/orange/g' data.txt > result_task1.txt
 # YOUR CODE HERE
+sed 's/apple/orange/g' data.txt > result_task1.txt
 
 # Task 2: data.txt から "#" で始まる行を除いて result_task2.txt に保存する
 # ヒント: sed '/^#/d' data.txt > result_task2.txt
 # YOUR CODE HERE
+sed '/^#/d' data.txt > result_task2.txt
 
 # Task 3: data.txt の1行目だけ抽出して result_task3.txt に保存する
 # ヒント: sed -n '1p' data.txt > result_task3.txt
 # YOUR CODE HERE
+sed -n '1p' data.txt > result_task3.txt
 
 # Task 4: data.txt の "apple" を "grape" に、"banana" を "melon" に連続置換して result_task4.txt に保存する
 # ヒント: sed -e 's/apple/grape/g' -e 's/banana/melon/g' data.txt > result_task4.txt
 # YOUR CODE HERE
+sed -e 's/apple/grape/g' -e 's/banana/melon/g' data.txt > result_task4.txt
 
 # Task 5: data.txt の2〜3行目だけ抽出して result_task5.txt に保存する
 # ヒント: sed -n '2,3p' data.txt > result_task5.txt
 # YOUR CODE HERE
+sed -n '2,3p' data.txt > result_task5.txt
